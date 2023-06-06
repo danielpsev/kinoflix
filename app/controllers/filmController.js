06e83a100082070e351b1c173b9e4c482b63660b
Daniel <danielpsev@gmail.com>
2023-06-07 00:29:27 +0300

    Film delete fix

--- app/controllers/filmController.js
+++ app/controllers/filmController.js
@@ -3,7 +3,14 @@ const User = require("../models/userModel");
 const { isValidObjectId } = require("mongoose");
 
 exports.getAllFilms = async (req, res) => {
-  const { page = 1, limit = 18, filter, sort_name, sort_type, title } = req.query;
+  const {
+    page = 1,
+    limit = 18,
+    filter,
+    sort_name,
+    sort_type,
+    title,
+  } = req.query;
 
   try {
     let filter_data = {};
@@ -12,14 +19,19 @@ exports.getAllFilms = async (req, res) => {
     }
 
     let sort = {};
-    if (sort_name === 'title' || sort_name === 'releaseYear' || sort_name === 'rating' || sort_name === 'createdAt' ) {
+    if (
+      sort_name === "title" ||
+      sort_name === "releaseYear" ||
+      sort_name === "rating" ||
+      sort_name === "createdAt"
+    ) {
       sort[sort_name] = sort_type;
     }
-    if(!sort_name){
-      sort['createdAt'] = sort_type ? sort_type : 'desc';
+    if (!sort_name) {
+      sort["createdAt"] = sort_type ? sort_type : "desc";
     }
     if (title) {
-      filter_data.title = { $regex: title, $options: 'i' };
+      filter_data.title = { $regex: title, $options: "i" };
     }
     let allFilms = await Film.find(filter_data)
       .sort(sort)
@@ -31,41 +43,43 @@ exports.getAllFilms = async (req, res) => {
 
     if (title && allFilms.length === 0) {
       return res.status(200).json({
-        status: 'error',
-        message: 'Film not found.'
+        status: "error",
+        message: "Film not found.",
       });
     }
 
-    if(req.userInfo){
+    if (req.userInfo) {
       const user = await User.findById(req.userInfo.id);
-      allFilms = allFilms.map(film => {
-        const isLiked = user.likes.some(like => like.film.toString() === film._id.toString());
+      allFilms = allFilms.map((film) => {
+        const isLiked = user.likes.some(
+          (like) => like.film.toString() === film._id.toString()
+        );
         return { ...film._doc, isLiked };
       });
     }
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
         films: allFilms,
         totalPages: Math.ceil(count / parseInt(limit)),
-        currentPage: parseInt(page)
-      }
+        currentPage: parseInt(page),
+      },
     });
   } catch (err) {
     res.status(500).json({
-      status: 'error',
-      message: err.message
+      status: "error",
+      message: err.message,
     });
   }
 };
 
 exports.getUserLikedFilms = async (req, res) => {
-  const { page = 1, limit = 12} = req.query;
+  const { page = 1, limit = 12 } = req.query;
   try {
     const likedFilms = await User.find()
       .populate({
-        path: 'likes.film',
+        path: "likes.film",
         model: "film",
         options: {
           limit: parseInt(limit),
@@ -73,67 +87,101 @@ exports.getUserLikedFilms = async (req, res) => {
         },
       })
       .exec();
-      const films = likedFilms
+    const films = likedFilms
       .map((user) =>
         user.likes.map((like) => ({
           ...like.film._doc,
           isLiked: true,
         }))
-      ).flat();
-      const count = await User.countDocuments().exec();
+      )
+      .flat();
+    const count = await User.countDocuments().exec();
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
         films: films,
         totalPages: Math.ceil(count / parseInt(limit)),
-        currentPage: parseInt(page)
-      }
+        currentPage: parseInt(page),
+      },
     });
-} catch (err) {
-  res.status(500).json({
-    status: 'error',
-    message: err.message
-  });
-}
-}
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
 exports.getFilm = async (req, res) => {
-    const { id } = req.params;
-    if (!isValidObjectId(id)) {
-        return res.status(404).json({ status: "error", mess: `Blogai nurodytas ID` });
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    return res
+      .status(404)
+      .json({ status: "error", mess: `Blogai nurodytas ID` });
+  }
+  try {
+    const film = await Film.findById(id);
+    if (!film) {
+      return res
+        .status(404)
+        .json({ status: "error", mess: `Filmas, id: ${id} neegzistuoja` });
+    } else {
+      let isLiked = false;
+      if (req.userInfo) {
+        const user = await User.findById(req.userInfo.id);
+        isLiked = user.likes.some((like) => like.film === id);
       }
-    try {
-      const film = await Film.findById(id);
-      if (!film) {
-        return res.status(404).json({ status: "error", mess: `Filmas, id: ${id} neegzistuoja` });
-      } else {
-        let isLiked = false;
-        if(req.userInfo){
-          const user = await User.findById(req.userInfo.id);
-          isLiked = user.likes.some(like => like.film === id);
-        }
-        const { likes, ...filmData } = film._doc;
-        const data = {...filmData, isLiked};
-        res.status(200).json(data);
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ status: "error", mess: err.message });
+      const { likes, ...filmData } = film._doc;
+      const data = { ...filmData, isLiked };
+      res.status(200).json(data);
     }
-  };
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "error", mess: err.message });
+  }
+};
 
 exports.addFilm = async (req, res) => {
   try {
-    const { title, description, genres, country, director, releaseYear, duration, rating, posterSrc, bgSrc, trailerID} = req.body;
-    const findFilm = await Film.findOne({title});
-    if(findFilm){
-      return res.status(400).json({status: "error", mess: "Filmas su tokiu pavadinimu jau egzistuoja" });
+    const {
+      title,
+      description,
+      genres,
+      country,
+      director,
+      releaseYear,
+      duration,
+      rating,
+      posterSrc,
+      bgSrc,
+      trailerID,
+    } = req.body;
+    const findFilm = await Film.findOne({ title });
+    if (findFilm) {
+      return res
+        .status(400)
+        .json({
+          status: "error",
+          mess: "Filmas su tokiu pavadinimu jau egzistuoja",
+        });
     }
-    const newFilm = await Film.create(
-      {title, description, genres, country, director, releaseYear, duration, rating, posterSrc, bgSrc, trailerID, type: "film"});
+    const newFilm = await Film.create({
+      title,
+      description,
+      genres,
+      country,
+      director,
+      releaseYear,
+      duration,
+      rating,
+      posterSrc,
+      bgSrc,
+      trailerID,
+      type: "film",
+    });
     res.status(201).json(newFilm);
   } catch (err) {
     console.log(err);
-    res.status(500).json({status: "error", mess: err });
+    res.status(500).json({ status: "error", mess: err });
   }
 };
 
@@ -142,30 +190,39 @@ exports.deleteFilm = async (req, res) => {
     const { id } = req.params;
     const delete_film = await Film.findById(id);
     if (!delete_film) {
-      return res.status(404).json({status: "error", mess: `Filmas nr: ${id} neegzistuoja` });
+      return res
+        .status(404)
+        .json({ status: "error", mess: `Filmas nr: ${id} neegzistuoja` });
     } else {
-        try {
-          await Film.findByIdAndDelete(id);
-          res.status(200).json({
-            status: "success",
-            message: `Filmas nr: ${id} sėkmingai pašalintas.`,
-            film: delete_film,
-          });
-        } catch (error) {
-          res.status(500).json({status: "error", mess: err });
-        }
+      try {
+        // Trinam filmą iš user'ių, kurie palikino
+        await User.updateMany(
+          { "likes.film": id },
+          { $pull: { likes: { film: id } } }
+        );
+
+        await Film.findByIdAndDelete(id);
+        res.status(200).json({
+          status: "success",
+          message: `Filmas nr: ${id} sėkmingai pašalintas.`,
+          film: delete_film,
+        });
+      } catch (error) {
+        res.status(500).json({ status: "error", mess: err });
+      }
     }
   } catch (err) {
-    res.status(500).json({status: "error", mess: err });
+    res.status(500).json({ status: "error", mess: err });
   }
 };
-
 
 exports.likeFilm = async (req, res) => {
   try {
     const { id } = req.params;
     if (!isValidObjectId(id)) {
-      return res.status(404).json({ status: "error", mess: `Blogai nurodytas ID` });
+      return res
+        .status(404)
+        .json({ status: "error", mess: `Blogai nurodytas ID` });
     }
 
     const film = await Film.findById(id);
@@ -176,8 +233,8 @@ exports.likeFilm = async (req, res) => {
     const user = await User.findById(req.userInfo.id);
     if (!user) {
       return res.status(404).json({
-        status: 'error',
-        message: 'Vartotojas nerastas.'
+        status: "error",
+        message: "Vartotojas nerastas.",
       });
     }
 
@@ -186,12 +243,12 @@ exports.likeFilm = async (req, res) => {
 
     if (userAlreadyLiked) {
       return res.status(400).json({
-        status: 'error',
-        message: 'Jau patinka.'
+        status: "error",
+        message: "Jau patinka.",
       });
     }
 
-    user.likes.push({ film: id, likedAt: new Date()});
+    user.likes.push({ film: id, likedAt: new Date() });
 
     await user.save();
 
@@ -209,7 +266,9 @@ exports.dislikeFilm = async (req, res) => {
   try {
     const { id } = req.params;
     if (!isValidObjectId(id)) {
-      return res.status(404).json({ status: "error", mess: `Blogai nurodytas ID` });
+      return res
+        .status(404)
+        .json({ status: "error", mess: `Blogai nurodytas ID` });
     }
 
     const film = await Film.findById(id);
@@ -220,21 +279,21 @@ exports.dislikeFilm = async (req, res) => {
     const user = await User.findById(req.userInfo.id);
     if (!user) {
       return res.status(404).json({
-        status: 'error',
-        mess: 'Vartotojas nerastas.'
+        status: "error",
+        mess: "Vartotojas nerastas.",
       });
     }
 
-    // Tikrinimas, ar filmId yra vartotojo likes masyve 
-    const likedIndex = user.likes.findIndex(like => like.film === id);
+    // Tikrinimas, ar filmId yra vartotojo likes masyve
+    const likedIndex = user.likes.findIndex((like) => like.film === id);
     if (likedIndex === -1) {
       return res.status(400).json({
-        status: 'error',
-        message: 'Film is not liked.'
+        status: "error",
+        message: "Film is not liked.",
       });
     }
 
-    // Filmo salinimas is vartotojo likes masyvo 
+    // Filmo salinimas is vartotojo likes masyvo
     user.likes.splice(likedIndex, 1);
     await user.save();
 
